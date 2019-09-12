ea56cafc9dedf9c5f4e352e053a0319c3b094455
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-09-12 14:54:34 -0700

    created api auth and user routes

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 5000;
