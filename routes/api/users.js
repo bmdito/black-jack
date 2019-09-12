e2e7aa1478806696153b3c1797aae9bb3546d629
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-09-12 16:44:01 -0700

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 //@route    POST api/users
 //@desc     Register user
@@ -17,12 +21,50 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      //gets the user
+      let user = await User.findOne({ email });
+      //if user exists..
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      //encrypt password with bycrpt
+      const salt = await bcrypt.genSalt(10);
+      //creates a hash and puts into user.password
+      user.password = await bcrypt.hash(password, salt);
+
+      //saves the user
+      await user.save();
+      //return json webtoken (need token for instant login with register)
+      res.send("User Registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
