93620917f1cc500b519753c9a83949b32d0b5888
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-09-12 15:00:45 -0700

    created user model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  //put this in user model so its always accessible
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    defualt: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
