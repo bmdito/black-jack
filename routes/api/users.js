5e71d5704aa2454568850a3410a4eb666e8681f5
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-09-13 17:53:50 -0700

    added jwt for register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -60,7 +62,22 @@ router.post(
       //saves the user
       await user.save();
       //return json webtoken (need token for instant login with register)
-      res.send("User Registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      //now that we have payload with user id...
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
