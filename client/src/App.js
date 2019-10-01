82bb9813091e7665f6d1d977aa90310ad62704ba
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-10-01 15:36:51 -0700

    table component layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
-import makeDeck from "./utils/deck";
+import Table from "./components/Table";
+import { Button } from "react-bootstrap";
+// import makeDeck from "./utils/deck";
 
 import "./App.css";
 
 function App() {
-  makeDeck();
+  // makeDeck();
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table />
+      {/* <Button bsStyle="success">Test</Button> */}
     </div>
   );
 }
--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,5 +1,33 @@
 import React, { Component } from "react";
+import container from "react-bootstrap";
+import "./style.css";
 
-class Table extends Component {}
+class Table extends Component {
+  render() {
+    return (
+      <>
+        <div className="container container-fluid">
+          <div className="row">
+            <div className="col-md-1"></div>
+            <div className="col-md-10">
+              <div className="table-visual">
+                <div className="dealerPos">dealer</div>
+                <div className="posOne">
+                  Pos1
+                  <div className="betOne betDiv">pos3</div>
+                </div>
+                <div className="posTwo">
+                  pos2
+                  <div className="betTwo betDiv">pos3</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-1"></div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default Table;
