fb44f5e9fd87763eb5128b5af87995c1a43708a3
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-10-14 15:24:36 -0700

    register layoutv.1

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Table from "./components/Table";
 import Landing from "./components/Landing/landing";
+import Register from "./components/auth/Register";
+
 import { Button } from "react-bootstrap";
 // import makeDeck from "./utils/deck";
 
@@ -9,10 +12,18 @@ import "./App.css";
 function App() {
   // makeDeck();
   return (
-    <div className="App">
-      <Landing />
-      {/* <Table /> */}
-    </div>
+    <Router>
+      <Fragment>
+        {/* navbar here */}
+        <Route exact path="/" component={Landing} />
+        <section className="App">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            {/* <Route exact path="/login" component={Login} /> */}
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
--- client/src/components/Landing/landing.js
+++ client/src/components/Landing/landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 class Landing extends Component {
@@ -6,7 +7,18 @@ class Landing extends Component {
     return (
       <>
         <div>Landing Page</div>
-        <div className="background-img"></div>
+        <div className="background-img">
+          <div className="inner">
+            <div className="buttons">
+              <Link to="/register" className="btn btn-primary">
+                Sign Up
+              </Link>
+              <Link to="/login" className="btn btn-light">
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
