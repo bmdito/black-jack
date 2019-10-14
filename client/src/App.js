82e3fd8bcc8b83ef7e9e29f04da7bf48da6693b1
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-10-14 13:55:17 -0700

    landing img and initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Table from "./components/Table";
+// import Table from "./components/Table";
+import Landing from "./components/Landing/landing";
 import { Button } from "react-bootstrap";
 // import makeDeck from "./utils/deck";
 
@@ -9,7 +10,8 @@ function App() {
   // makeDeck();
   return (
     <div className="App">
-      <Table />
+      <Landing />
+      {/* <Table /> */}
     </div>
   );
 }
--- client/src/components/Landing/landing.js
+++ client/src/components/Landing/landing.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import "./style.css";
 
 class Landing extends Component {
   render() {
     return (
       <>
         <div>Landing Page</div>
+        <div className="background-img"></div>
       </>
     );
   }
