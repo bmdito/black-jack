19d93f18b66a7b8a079940c56ae4d52813bdc6bd
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-11-05 16:10:53 -0800

    updated for heroku

---
+++ client/build/precache-manifest.de59f63d7f680f3d114d97eacee01c3f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eec5634aef9550169a0f13ae30ca7dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe82bcaa70911e2460f4",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "bfcbb24705550ad960f4",
+    "url": "/static/css/main.58d0ffe5.chunk.css"
+  },
+  {
+    "revision": "fe82bcaa70911e2460f4",
+    "url": "/static/js/2.c91bcabc.chunk.js"
+  },
+  {
+    "revision": "bfcbb24705550ad960f4",
+    "url": "/static/js/main.be2942b7.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "255639e99b0a90d5a3b55136b4e19447",
+    "url": "/static/media/ace-of-spades.255639e9.jpg"
+  },
+  {
+    "revision": "8698b6a0b143f311d361a5a22bb072f0",
+    "url": "/static/media/card-back.8698b6a0.png"
+  },
+  {
+    "revision": "77576e0eabb2df699e9ed21e19700015",
+    "url": "/static/media/card-border.77576e0e.jpg"
+  },
+  {
+    "revision": "403d595c745743a456f884699a3a39e0",
+    "url": "/static/media/pokerchips.403d595c.png"
+  }
+]);
